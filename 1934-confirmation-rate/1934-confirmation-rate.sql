9ad5c79ef541e29f645340c5f4c9f0cf7a008947
Sakshi Bute <sakshibute44@gmail.com>
2026-08-23 22:26:25 +0530

    Time: 703 ms (64.26%), Space: 0B (100%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -2,6 +2,19 @@
 
 
 
+-- SELECT 
+--     user_id,
+--     -- Step 1: Calculate the confirmation rate
+--     ROUND(
+--         SUM(CASE WHEN action = 'confirmed' THEN 1 ELSE 0 END) 
+--         / 
+--         COUNT(*), 
+--         2
+--     ) AS confirmation_rate
+-- FROM Confirmations
+-- GROUP BY user_id;
+
+
 SELECT 
     s.user_id,
     ROUND(IFNULL(AVG(CASE WHEN c.action = 'confirmed' THEN 1 ELSE 0 END), 0), 2) AS confirmation_rate
